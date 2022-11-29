57a97b7f6aec76cb78642489c77197dd1e9e8306
gwangho-y <yigwangho21@gmail.com>
2022-11-29 11:11:27 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/get',//api request path
+        destination: 'https://a0.muscache.com/',//목적 path
+      },
+    ]
+  },
+  images:{
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'links.papareact.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'a0.muscache.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    // domains: ['links.papareact.com', 'a0.muscache.com'],
+  }
 }
 
 module.exports = nextConfig
